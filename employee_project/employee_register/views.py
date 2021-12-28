a8add4347fc22acf5441b5ff39d9bc179f68f6b3
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-12-28 20:16:25 +0530

    Views modified

--- employee_project/employee_register/views.py
+++ employee_project/employee_register/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def employee_list(request):
-    return
+    return render(request,"employee_register/employee_list.html")
 
 def employee_form(request):
-    return
+    return render(request,"employee_register/employee_form.html")
 
 def employee_delete(request):
     return

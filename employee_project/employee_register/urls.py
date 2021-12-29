07661d51ae7085143c8d0940c07686ab58ba753b
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-12-30 00:07:13 +0530

    Update route added

--- employee_project/employee_register/urls.py
+++ employee_project/employee_register/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.employee_form),
+    path('<int:id>/', views.employee_form, name = 'employee_update'),
     path('list/',views.employee_list)
 ]

5a1131fed474f5b1183ade6609ac6c3cecdcdf51
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-12-29 16:35:25 +0530

    Route modified

--- employee_project/employee_register/views.py
+++ employee_project/employee_register/views.py
@@ -1,17 +1,24 @@
 from django import forms
 from django.forms.forms import Form
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from employee_register.forms import EmployeeForm
 from .forms import EmployeeForm
+from django.shortcuts import render,redirect
 # Create your views here.
 
 def employee_list(request):
     return render(request,"employee_register/employee_list.html")
 
 def employee_form(request):
-    form = EmployeeForm()
-    return render(request,"employee_register/employee_form.html",{'form':form})
+    if request.method == "GET":
+         form = EmployeeForm()
+         return render(request,"employee_register/employee_form.html",{'form':form})
+    else:
+         form = EmployeeForm(request.POST)
+         if form.is_valid:
+            form.save()
+         return redirect('/employee/list')
 
 def employee_delete(request):
     return

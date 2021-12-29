fb5913ec79dee5e8e3061542c8882cfed0d7c7aa
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-12-29 15:10:33 +0530

    Bootstrap added

--- employee_project/employee_register/forms.py
+++ employee_project/employee_register/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from django.db.models.base import Model
 from django.forms import fields
 from .models import Employee
-
+from django import forms
 
 class EmployeeForm(forms.ModelForm):
      
      class Meta:
          model = Employee
-         fields = '__all__'
+         fields = ('fullname','emp_code','mobile','position')
+         labels = {
+             'fullname' : 'Full Name',
+             'emp_code' : 'Employee Code',
+             'mobile' : 'Mobile Number',
+             'position' : 'Position',
+         }

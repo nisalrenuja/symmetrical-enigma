af6ab707173dbf20dec6b4d50f4ff36a443f9b26
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-12-30 02:42:54 +0530

    Delete route added

--- employee_project/employee_register/urls.py
+++ employee_project/employee_register/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.employee_form,name = 'employee_insert'), #get and post request for insert operation
     path('<int:id>/', views.employee_form, name = 'employee_update'), #get and post request for update operation
-    path('list/',views.employee_list,name = 'employee_list') #get and view all the list of employees
+    path('list/',views.employee_list,name = 'employee_list'), #get and view all the list of employees
+    path('delete/<int:id>/',views.employee_delete,name= 'employee_delete') #delete relevent employee 
 ]

229a9c486e131ebc4fa6c69178aed7453caa50b3
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-12-30 00:33:05 +0530

    Routers updated

--- employee_project/employee_register/urls.py
+++ employee_project/employee_register/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form),
-    path('<int:id>/', views.employee_form, name = 'employee_update'),
-    path('list/',views.employee_list)
+    path('', views.employee_form,name = 'employee_insert'), #get and post request for insert operation
+    path('<int:id>/', views.employee_form, name = 'employee_update'), #get and post request for update operation
+    path('list/',views.employee_list,name = 'employee_list') #get and view all the list of employees
 ]
